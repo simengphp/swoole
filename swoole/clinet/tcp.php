4d35ee2dc841c7c5b839f1d6b8743b6352607dc5
liuzhu <liuzhu@zanpu.com>
2018-09-10 15:14:56 +0800

    swoole创建tcp客户端

---
+++ swoole/clinet/tcp.php
@@ -0,0 +1,21 @@
+<?php
+$client = new swoole_clinet(SWOOLE_TCP);
+
+if (!$client->connect('127.0.0.1', 9501)) {
+    echo 'connect fail';
+    exit();
+}
+
+fwrite(STDOUT, '请输入你要发送的信息');
+
+$msg = trim(fgets(STDIN));
+
+$int = $client->send($msg);
+
+if (!$int) {
+    echo '发送失败';
+    exit();
+}
+
+$result = $client->recv();
+echo $result;
